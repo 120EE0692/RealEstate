33cb18656ca51c5bebe880c84b2f929ffb265a6e
Ashish Pradhan <ashishpradhan2345@gmail.com>
2021-12-13 22:12:00 +0530

    reolve utils/fetchApi in [id].js 01

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -4,7 +4,7 @@ import { BsGridFill } from "react-icons/bs";
 import { GoVerified } from "react-icons/go";
 import millify from "millify";
 
-import { baseUrl, fetchApi } from "../../utils/fetchAPI(rename)";
+import { baseUrl, fetchApi } from "../../utils/fetchApi";
 import ImageScrollbar from "../../components/ImageScrollbar";
 
 const PropertyDetails = ({
