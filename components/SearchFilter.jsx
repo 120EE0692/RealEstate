186add741e4170c31adfc538217909f1798a8d82
Ashish Pradhan <ashishpradhan2345@gmail.com>
2021-12-13 20:03:33 +0530

    Loading Progress

--- components/SearchFilter.jsx
+++ components/SearchFilter.jsx
@@ -1,5 +1,15 @@
 import { useEffect, useState } from "react";
-import { Flex, Box, Input, Text, Button, Spinner, Icon, Select, } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Input,
+  Text,
+  Button,
+  Spinner,
+  Icon,
+  Select,
+  filter,
+} from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { MdCaricel } from "react-icons/md";
 import Image from "next/image";
@@ -9,21 +19,33 @@ import { filterData, getFilterValues } from "../utils/filterData";
 const SearchFilters = () => {
   const [filters, setFilters] = useState(filterData);
   const router = useRouter();
-  
+
   const searchProperties = (filterValues) => {
-      const path = router.pathname;
-      const {query} = router;
-      const values = getFilterValues(filterValues);
-      values.forEach((item)=>{query[item.name] = item.value;})
-      router.push({pathname:path,query})
+    const path = router.pathname;
+    const { query } = router;
+    const values = getFilterValues(filterValues);
+    values.forEach((item) => {
+      if(item.value && filterValues?.[item.name]){
+        query[item.name] = item.value;
+      }
+
+      
+    });
+    router.push({ pathname: path, query });
   };
 
   return (
     <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
       {filters.map((filter) => (
         <Box key={filter.queryName}>
-          <Select placeholder={filter.placeholder} w="fit-content" p="2"
-            onChange={(e) => searchProperties({ [filter.queryName]: e.target.value }) } >
+          <Select
+            placeholder={filter.placeholder}
+            w="fit-content"
+            p="2"
+            onChange={(e) =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            }
+          >
             {filter?.items?.map((item) => (
               <option value={item.value} key={item.value}>
                 {item.name}
